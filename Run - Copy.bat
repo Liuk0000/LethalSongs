d610bd7b1529cc275ec7208e83a3f41145644ca0
Luca De Bellis <30722675+Liuk0000@users.noreply.github.com>
2024-12-26 17:35:04 +0100

    changed run files names

--- Run - Copy.bat
+++
@@ -1,1 +0,0 @@
-powershell -ExecutionPolicy Bypass -File "LethalSongs.ps1"
--- Run.bat
+++ Run.bat
@@ -1,1 +1,1 @@
-powershell -ExecutionPolicy Bypass -File "LethalSongs.ps1" "xao0ey3bpdc011fp" "R" "Test"
+powershell -ExecutionPolicy Bypass -File "LethalSongs.ps1"
